6cd93f7cc4d98b737022a9b939b45592af78920b
mznaturl1 <117309455+mznaturl1@users.noreply.github.com>
2023-01-18 23:17:31 -0500

    Add files via upload

---
+++ PyBank/main.py
@@ -0,0 +1,70 @@
+import os
+
+import csv
+
+#Open CSV as Reader 
+with open ("C:/Users/msmic/OneDrive/Desktop/python-challenge/PyBank/Resources/budget_data.csv") as csvfile:
+    csv_reader=csv.reader(csvfile, delimiter=",")
+    csv_header=next(csvfile)
+    print(f"Header:{csv_header}")
+
+#Declare Variables
+
+    monthcount=0
+    total=0
+    average_change=0
+    date=[]
+    profits=[]
+    previous_profit=0
+    difference=[]
+    total_difference=0
+    increase={"month": "", "profits": 0}
+    decrease={"month": "", "losses": 0}
+    
+#The total number of months included in the dataset
+    for row in csv_reader:
+        monthcount +=1
+        total += int(row[1])
+
+#The net total amount of "Profit/Losses" over the entire period
+        date.append(row[0])
+        profit = int(row[1])
+        profits.append(profit)
+    
+#Calculate the changes in "Profit/Losses" over the entire period, 
+        cd=0
+        if previous_profit != 0: 
+            cd = profit - previous_profit
+            difference.append(cd)
+        previous_profit = profit
+        
+#The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in losses (date and amount) over the entire period      
+  
+        if cd > increase['profits']:
+            increase['profits'] = cd
+            increase['month'] = row[0]
+        if cd < decrease['losses']:
+            decrease['losses'] = cd
+            decrease['month'] = row[0]
+
+#Average of changes       
+ 
+    average_change = sum(difference) / len(difference)
+    
+print(f"Financial Analysis")
+print(f"----------------------------")
+print(f"Total Months: {monthcount}")
+print(f"Total: ${total}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {increase['month']} ${increase['profits']}")
+print(f"Greatest Decrease in Profits: {decrease['month']} ${decrease['losses']}")
+
+output = open("PyBank/Analysis/file.txt","w")
+output.write(f"Financial Analysis\n")
+output.write(f"----------------------------\n")
+output.write(f"Total Months: {monthcount}\n")
+output.write(f"Total: ${total}\n")
+output.write(f"Average Change: ${average_change:.2f}\n")
+output.write(f"Greatest Increase in Profits: {increase['month']} ${increase['profits']}\n")
+output.write(f"Greatest Decrease in Profits: {decrease['month']} ${decrease['losses']}\n")
---
+++ PyPoll/main.py
@@ -0,0 +1,61 @@
+import csv
+output = open("PyPoll/Analysis/file.txt","w")
+csvpath= 'PyPoll/Resources/election_data.csv'
+
+#Open CSV as Reader
+with open(csvpath) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ",")
+    csv_header = next(csv_file)
+    print(f"Header:{csv_header}")
+    output.write(f"Header:{csv_header}\n")
+
+#Declare Variables
+    votescast=0
+    totalvotescast=0
+    candidates=[]
+    numberwon = {}
+    percentwon=0
+    winnername=""
+    winnervotes=0
+  
+#The total number of votes cast
+    for row in csv_reader:
+        votescast +=1
+    
+#A complete list of candidates who received votes
+        if row[2] not in candidates:
+            candidates.append(row[2])
+#The total number of votes each candidate won
+            numberwon[row[2]]=0
+        numberwon[row[2]]+=1
+        
+#Print
+print(f"Election Results")
+print(f"-------------------------")
+print(f"Total Votes: {votescast}")
+print(f"-------------------------")
+output.write(f"Election Results\n")
+output.write(f"-------------------------\n")
+output.write(f"Total Votes: {votescast}\n")
+output.write(f"-------------------------\n")
+
+#The percentage of votes each candidate won
+for name in numberwon:
+    votes = numberwon[name]
+    percentage = votes / votescast
+    pretty = percentage * 100
+    print(f"{name}: {pretty:.3f}% ({votes})")
+    output.write(f"{name}: {pretty:.3f}% ({votes})\n")
+
+#The winner of the election based on popular vote.
+    if votes > winnervotes: 
+        winnervotes = votes
+        winnername = name
+
+#Print 
+print(f"-------------------------")
+print(f"Winner: {winnername}")
+print(f"-------------------------")
+output.write(f"-------------------------\n")
+output.write(f"Winner: {winnername}\n")
+output.write(f"-------------------------\n")
